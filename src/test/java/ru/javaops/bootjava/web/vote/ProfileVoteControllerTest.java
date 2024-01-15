6639e03d4aaf8495d011199db67d63e41a0b3657
olgademirtash <olga.demirtash@gmail.com>
2024-01-16 00:44:08 +0300

    vote test updates

--- src/test/java/ru/javaops/bootjava/web/vote/ProfileVoteControllerTest.java
+++ src/test/java/ru/javaops/bootjava/web/vote/ProfileVoteControllerTest.java
@@ -6,21 +6,25 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.bootjava.repository.VoteRepository;
 import ru.javaops.bootjava.to.VoteTo;
 import ru.javaops.bootjava.util.JsonUtil;
 import ru.javaops.bootjava.util.VotesUtil;
 import ru.javaops.bootjava.web.AbstractControllerTest;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.javaops.bootjava.web.restaurant.RestaurantTestData.restaurant1;
 import static ru.javaops.bootjava.web.restaurant.RestaurantTestData.restaurant3;
 import static ru.javaops.bootjava.web.user.UserTestData.USER_MAIL;
 import static ru.javaops.bootjava.web.user.UserTestData.user;
+import static ru.javaops.bootjava.web.vote.VoteController.REST_URL;
 import static ru.javaops.bootjava.web.vote.VoteTestData.*;
 
 class ProfileVoteControllerTest extends AbstractControllerTest {
-    private static final String REST_URL_SLASH = VoteController.REST_URL + '/';
+    private static final String REST_URL_SLASH = REST_URL + '/';
 
     @Autowired
     private VoteRepository voteRepository;
@@ -42,7 +46,7 @@ class ProfileVoteControllerTest extends AbstractControllerTest {
     @WithUserDetails(value = USER_MAIL)
     void create() throws Exception {
         VoteTo newVoteTo = VotesUtil.createTo(getNew(user, restaurant1));
-        ResultActions action = perform(MockMvcRequestBuilders.post(VoteController.REST_URL)
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtil.writeValue(newVoteTo)));
 
@@ -55,15 +59,13 @@ class ProfileVoteControllerTest extends AbstractControllerTest {
 
     @Test
     @WithUserDetails(value = USER_MAIL)
-    void createInvalid() throws Exception {
-        VoteTo newVoteTo = VotesUtil.createTo(getNew(user, restaurant1));
-        ResultActions action = perform(MockMvcRequestBuilders.post(VoteController.REST_URL)
+    @Transactional(propagation = Propagation.NEVER)
+    void createDuplicate() throws Exception {
+        VoteTo invalid = VotesUtil.createTo(getNew(user, restaurant1));
+        perform(MockMvcRequestBuilders.post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(newVoteTo)));
-
-        VoteTo created = VOTE_TO_MATCHER.readFromJson(action);
-        int newId = created.getId();
-        newVoteTo.setId(newId);
-        VOTE_TO_MATCHER.assertMatch(created, newVoteTo);
+                .content(JsonUtil.writeValue(invalid)))
+                .andDo(print())
+                .andExpect(status().isConflict());
     }
 }
